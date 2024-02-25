50b1cd1138a52cfea11efe4f0a91be42f02e69f8
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-25 03:42:07 +0000

    Use quickjs

--- mod.ts
+++ mod.ts
@@ -1,15 +1,14 @@
 // Copyright 2022- TANIGUCHI Masaya
 // MIT License https://git.io/mit-license
-
-import Sval from "https://esm.sh/sval@v0.4.8?bundle&no-check";
+import { getQuickJS } from "npm:quickjs-emscripten@0.29.0";
 import { escapeHtml } from "https://deno.land/x/escape@1.4.2/mod.ts";
 
-export type Template = (globalThis: Record<string, unknown>) => Promise<string>;
+export type Template = (globalThis?: Record<string, unknown>) => Promise<string>;
 type State = "open" | "close" | "escaped" | "unescaped";
 
 export const compile = (src: string): Template => {
   const tokens = src.split(/(<%[-=]?|%>)/);
-  const lines: string[] = ["(async () => {", "let __result=''"];
+  const lines: string[] = ["let __result=''"];
   let state: State = "close";
   for (const token of tokens) {
     switch (token) {
@@ -37,25 +36,24 @@ export const compile = (src: string): Template => {
         lines.push(`__result+=__escape(${token})`);
         break;
       case "close":
-        lines.push(`__result+=atob("${btoa(token)}")`);
+        lines.push(`__result+=__atob("${btoa(token)}")`);
         break;
     }
   }
-  lines.push("__resolve(__result)", "})()");
+  lines.push("__result");
   const code = lines.join(";\n");
-  return (globalThis: Record<string, unknown>) =>
-    new Promise((resolve) => {
-      const js = new Sval();
-      js.import(globalThis);
-      js.import({
-        __resolve: resolve,
-        __escape: escapeHtml,
-      });
-      try {
-        js.run(code);
-      } catch (err) {
-        console.error(code);
-        throw err;
-      }
-    });
+  return async (globalThis: Record<string, unknown> = {}) => {
+    const QuickJS = await getQuickJS();
+    using vm = QuickJS.newContext();
+    using __escape = vm.newFunction("__escape", (str) => (vm.newString(escapeHtml(vm.dump(str)))));
+    vm.setProp(vm.global, "__escape", __escape);
+    using __atob = vm.newFunction("__atob", (str) => (vm.newString(atob(vm.dump(str)))));
+    vm.setProp(vm.global, "__atob", __atob);
+    for (const [key, value] of Object.entries(globalThis)) {
+      using jsValue = vm.unwrapResult(vm.evalCode(JSON.stringify(value)));
+      vm.setProp(vm.global, key, jsValue);
+    }
+    using result = vm.unwrapResult(vm.evalCode(code));
+    return vm.dump(result);
+  }
 };
